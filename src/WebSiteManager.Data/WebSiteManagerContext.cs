ec6e1af2fdc8044585e1193a476ffe596109f856
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-10 22:54:04 +0300

    Added table constraints

--- src/WebSiteManager.Data/WebSiteManagerContext.cs
+++ src/WebSiteManager.Data/WebSiteManagerContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
 using WebSiteManager.DataModels;
 
 namespace WebSiteManager.Data
@@ -13,6 +16,57 @@ namespace WebSiteManager.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder
+                .Entity<Category>(entityTypeBuilder =>
+                {
+                    entityTypeBuilder.Property(e => e.Id)
+                        .HasConversion<byte>();
+
+                    entityTypeBuilder.Property(e => e.Name)
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    entityTypeBuilder.HasData(Enum.GetValues(typeof(CategoryId))
+                        .Cast<CategoryId>()
+                        .Select(e => new Category
+                        {
+                            Id = e,
+                            Name = e.ToString().Humanize(LetterCasing.Sentence)
+                        })
+                    );
+                });
+
+            modelBuilder
+                .Entity<WebSite>(entityTypeBuilder =>
+                {
+                    entityTypeBuilder.Property(e => e.Name)
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    entityTypeBuilder.Property(e => e.Url)
+                        .IsRequired()
+                        .HasMaxLength(200);
+
+                    entityTypeBuilder.Property(e => e.IsDeleted)
+                        .IsRequired()
+                        .HasDefaultValue(false);
+
+                    entityTypeBuilder.Property(e => e.CategoryId)
+                        .HasConversion<byte>();
+                });
+
+            modelBuilder
+                .Entity<Login>(entityTypeBuilder =>
+                {
+                    entityTypeBuilder.Property(e => e.Username)
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    entityTypeBuilder.Property(e => e.Password)
+                        .IsRequired()
+                        .HasMaxLength(100);
+                });
+
             base.OnModelCreating(modelBuilder);
         }
     }
