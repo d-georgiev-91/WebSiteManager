63683bda1c100675cc07ebbc70d2ebfa7d41d9e3
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-10 23:40:19 +0300

    Added snapshot path url

---
+++ src/WebSiteManager.Data/Migrations/20200610203842_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebSiteManager.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 100, nullable: false),
+                    Password = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebSites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Url = table.Column<string>(maxLength: 200, nullable: false),
+                    HomePageSnapshotPath = table.Column<string>(maxLength: 300, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CategoryId = table.Column<byte>(nullable: false),
+                    LoginId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebSites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebSites_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WebSites_Logins_LoginId",
+                        column: x => x.LoginId,
+                        principalTable: "Logins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { (byte)1, "Arts and entertainment" },
+                    { (byte)2, "E commerce" },
+                    { (byte)3, "Finance" },
+                    { (byte)4, "Home and garden" },
+                    { (byte)5, "Jobs and career" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebSites_CategoryId",
+                table: "WebSites",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebSites_LoginId",
+                table: "WebSites",
+                column: "LoginId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebSites");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Logins");
+        }
+    }
+}
