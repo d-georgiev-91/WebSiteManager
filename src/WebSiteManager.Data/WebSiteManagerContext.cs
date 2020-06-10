8cffdef1c071876bae7d64c0a6cbc6893905f5f5
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-10 16:00:50 +0300

    Added Login entity

--- src/WebSiteManager.Data/WebSiteManagerContext.cs
+++ src/WebSiteManager.Data/WebSiteManagerContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebSiteManager.DataModels;
 
 namespace WebSiteManager.Data
 {
     public class WebSiteManagerContext : DbContext
     {
-
+        public DbSet<Login> Logins { get; set; }
     }
 }
---
+++ src/WebSiteManager.DataModels/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace WebSiteManager.DataModels
+{
+    public class Login
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
