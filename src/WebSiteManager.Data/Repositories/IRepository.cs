b23a2bcd1ba9be5f9e1042b513e09016b0d611ea
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-12 00:45:20 +0300

    Added Repository

---
+++ src/WebSiteManager.Data/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WebSiteManager.Data.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> Get();
+
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+    }
+}
---
+++ src/WebSiteManager.Data/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebSiteManager.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get() => _dbSet;
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate) => _dbSet.Where(predicate);
+
+        public void Add(TEntity entity) => ChangeState(entity, EntityState.Added);
+
+        public void Delete(TEntity entity) => ChangeState(entity, EntityState.Deleted);
+
+        public void Update(TEntity entity) => ChangeState(entity, EntityState.Modified);
+
+        private void ChangeState(TEntity entity, EntityState state)
+        {
+            var entry = _dbContext.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+
+            entry.State = state;
+        }
+
+        public void Dispose() => _dbContext?.Dispose();
+    }
+}
