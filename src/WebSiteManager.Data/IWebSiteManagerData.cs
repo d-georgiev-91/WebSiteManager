fcf93a41b117d98a87587fda8fc8aee7e9bb902c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-12 01:00:21 +0300

    Added and implemented IWebSiteManagerData representing unit of work

---
+++ src/WebSiteManager.Data/IWebSiteManagerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using WebSiteManager.Data.Repositories;
+using WebSiteManager.DataModels;
+
+namespace WebSiteManager.Data
+{
+    public interface IWebSiteManagerData
+    {
+        IRepository<WebSite> WebSiteRepository { get; }
+
+        IRepository<Login> LoginRepository { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/WebSiteManager.Data/WebSiteManagerData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebSiteManager.Data.Repositories;
+using WebSiteManager.DataModels;
+
+namespace WebSiteManager.Data
+{
+    public class WebSiteManagerData : IWebSiteManagerData
+    {
+        private readonly WebSiteManagerContext _dbContext;
+        private Dictionary<Type, object> _repositories;
+
+        public WebSiteManagerData(WebSiteManagerContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IRepository<WebSite> WebSiteRepository => GetRepository<WebSite>();
+
+        public IRepository<Login> LoginRepository => GetRepository<Login>();
+
+        private IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            _repositories ??= new Dictionary<Type, object>();
+
+            var type = typeof(TEntity);
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new Repository<TEntity>(_dbContext);
+                _repositories.Add(type, repository);
+            }
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
