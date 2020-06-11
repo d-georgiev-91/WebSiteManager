2fdb0a6f23ef2cb16f6224b80ef4ba131ab430e0
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-12 01:19:21 +0300

    Added add and delete methods and WebSiteService

---
+++ src/WebSiteManager.Services/IWebSiteService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using WebSiteManager.DataModels;
+
+namespace WebSiteManager.Services
+{
+    public interface IWebSiteService
+    {
+        Task AddAsync(WebSite webSite);
+
+        Task DeleteAsync(int webSiteId);
+    }
+}
---
+++ src/WebSiteManager.Services/WebSiteService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using WebSiteManager.Data;
+using WebSiteManager.DataModels;
+
+namespace WebSiteManager.Services
+{
+    public class WebSiteService : IWebSiteService
+    {
+        private readonly IWebSiteManagerData _webSiteManagerData;
+
+        public WebSiteService(IWebSiteManagerData webSiteManagerData)
+        {
+            _webSiteManagerData = webSiteManagerData;
+        }
+
+        public async Task AddAsync(WebSite webSite)
+        {
+            //TODO: Store image and crypt password
+            _webSiteManagerData.WebSiteRepository.Add(webSite);
+            await _webSiteManagerData.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int webSiteId)
+        {
+            var webSite = _webSiteManagerData.WebSiteRepository.Get(p=> p.Id == webSiteId).FirstOrDefault();
+            webSite.IsDeleted = false;
+            await _webSiteManagerData.SaveChangesAsync();
+        }
+    }
+}
