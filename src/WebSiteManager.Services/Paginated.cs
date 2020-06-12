32386b8cacc8492478287db8927e4ebdf8378613
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-13 02:19:51 +0300

    Added class for returning Paginated collection

---
+++ src/WebSiteManager.Services/Paginated.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebSiteManager.Services
+{
+    public class Paginated<TData>
+    {
+        public IEnumerable<TData> Data { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
