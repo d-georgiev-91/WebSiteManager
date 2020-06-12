028a68072b24411bc8117508dea90e760be0474b
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-13 02:18:49 +0300

    Added page class for Page input parameters

---
+++ src/WebSiteManager.Services/Page.cs
@@ -0,0 +1,15 @@
+﻿namespace WebSiteManager.Services
+{
+    public class Page
+    {
+        /// <summary>
+        /// Page index zero based
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Page size
+        /// </summary>
+        public int Size { get; set; }
+    }
+}
