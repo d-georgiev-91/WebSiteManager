10f80fa3de5c9a39c0242e823405b74ddcced7ac
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-13 11:18:30 +0300

    Added web application

---
+++ src/WebSiteManager.Web/Controllers/WebSitesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSiteManager.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebSitesController : ControllerBase
+    {
+        public WebSitesController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok("test");
+        }
+    }
+}
